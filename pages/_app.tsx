c918858d92f5b905eab505d76b60be4e3053b114
Joshua Davian <joshuadavian@gmail.com>
2022-03-30 10:43:16 +0700

    slicing detail page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/utilities.css';
 import '../styles/homepage.css';
+import '../styles/detail.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
@@ -21,14 +22,16 @@ function MyApp({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
         {/* AOS Animation */}
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+        <link
+          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+          rel="stylesheet"
+        />
         {/* Bootstrap JS */}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
           crossOrigin="anonymous"
         />
-
       </Head>
       <Component {...pageProps} />
     </>
