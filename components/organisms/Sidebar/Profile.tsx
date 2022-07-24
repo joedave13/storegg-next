30630224a80d452c0520aa46d519607d66374214
Joshua Davian <joshuadavian@gmail.com>
2022-07-24 14:42:18 +0700

    member sidebar user information integration

--- components/organisms/Sidebar/Profile.tsx
+++ components/organisms/Sidebar/Profile.tsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+import { JWTPayloadTypes, UserTypes } from '../../../services/data-types';
+
 export default function Profile() {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    avatar: '',
+  });
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userData: UserTypes = payload.player;
+      const IMG = process.env.NEXT_PUBLIC_IMAGE;
+      userData.avatar = `${IMG}/player/${userData.avatar}`;
+      setUser(userData);
+    }
+  }, []);
+
   return (
     <div className="user text-center pb-50 pe-30">
       <img
-        src="/img/avatar-1.png"
+        src={user.avatar}
         width="90"
         height="90"
         className="img-fluid mb-20"
-        alt=""
+        alt="Profile"
+        style={{ borderRadius: '100%' }}
       />
-      <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-      <p className="color-palette-2 m-0">shayna@anne.com</p>
+      <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
+      <p className="color-palette-2 m-0">{user.email}</p>
     </div>
   );
 }
