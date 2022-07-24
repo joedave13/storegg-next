bafb73c9be403be8b31ca84fb8a351cd36d066cd
Joshua Davian <joshuadavian@gmail.com>
2022-07-24 14:29:52 +0700

    member private route

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -1,5 +1,7 @@
+import jwtDecode from 'jwt-decode';
 import OverviewContent from '../../components/organisms/OverviewContent';
 import Sidebar from '../../components/organisms/Sidebar';
+import { JWTPayloadTypes, UserTypes } from '../../services/data-types';
 
 export default function Member() {
   return (
@@ -8,4 +10,36 @@ export default function Member() {
       <OverviewContent />
     </section>
   );
+}
+
+interface GetServerSideProps {
+  req: {
+    cookies: {
+      token: string;
+    };
+  };
+}
+
+export async function getServerSideProps({ req }: GetServerSideProps) {
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+
+  const jwtToken = Buffer.from(token, 'base64').toString('ascii');
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+  const userData: UserTypes = payload.player;
+  const IMG = process.env.NEXT_PUBLIC_IMAGE;
+  userData.avatar = `${IMG}/player/${userData.avatar}`;
+
+  return {
+    props: {
+      user: userData,
+    },
+  };
 }
